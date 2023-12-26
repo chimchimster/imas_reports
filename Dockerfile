f24d6d568ec42c284d5a317b7a2848abf18fd170
chimchimster <jorddyishere@gmail.com>
2023-12-26 12:06:55 +0600

    add k8s deployment

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR reports/
 COPY requirements.txt .
 RUN pip install -r requirements.txt --no-cache-dir
 COPY . .
-VOLUME reports/volumes/:/modules/apps/word/merged
+VOLUME ./reports/volumes/:/modules/apps/word/merged
 ENTRYPOINT ["sh", "app.sh"]
--- server/namespace/hook.py
+++ server/namespace/hook.py
@@ -1,11 +1,12 @@
+import time
+import shutil
 import os.path
 import multiprocessing
-import shutil
-import time
-
-from reports.redis_api.api import ReportStorageRedisAPI
 
 from flask_socketio import Namespace, emit
+
+from reports.redis_api.api import ReportStorageRedisAPI
+from modules.logs.handlers import LokiLogger
 
 
 class SocketWebHookNamespace(Namespace):
@@ -41,46 +42,50 @@ class SocketWebHookNamespace(Namespace):
 
     def __check_for_key_in_redis(self, msg: str, q: multiprocessing.Queue, timeout: int = 5000) -> bool:
 
-        with self.sema:
-            if self.r_con is not None:
-                start = end = time.time()
-                while end - start < timeout:
-                    has_key = self.r_con.connection.get(msg)
-                    if has_key:
-                        q.put(True)
-                        return True
-                    end = time.time()
-            q.put(False)
-            return False
+        with LokiLogger('Trying to find in Redis', report_id=msg):
+            with self.sema:
+                if self.r_con is not None:
+                    start = end = time.time()
+                    while end - start < timeout:
+                        has_key = self.r_con.connection.get(msg)
+                        if has_key:
+                            q.put(True)
+                            return True
+                        end = time.time()
+                q.put(False)
+                return False
 
     def __send_file_to_client(self, filename: str, q: multiprocessing.Queue):
 
-        with self.sema:
-            try:
-                with open(
-                        os.path.join(
-                            self.path_to_file_dir,
-                            filename,
-                            filename + '.docx',
-                        ), 'rb') as file:
-                    file_data = file.read()
-                    emit('message', {'file_data': file_data})
-                    q.put(True)
-            except FileNotFoundError:
-                q.put(False)
+        with LokiLogger('Sending file to client', report_id=filename):
+            with self.sema:
+                try:
+                    with open(
+                            os.path.join(
+                                self.path_to_file_dir,
+                                filename,
+                                filename + '.docx',
+                            ), 'rb') as file:
+                        file_data = file.read()
+                        emit('message', {'file_data': file_data})
+                        q.put(True)
+                except FileNotFoundError:
+                    q.put(False)
 
     def __fork_on_message(self, msg: str):
 
-        multiprocessing.active_children()
+        prc_count = multiprocessing.active_children()
 
-        mul_queue = multiprocessing.Queue()
-        prc_redis = multiprocessing.Process(target=self.__check_for_key_in_redis, args=(msg, mul_queue))
-        prc_redis.start()
+        with LokiLogger('Tracking processes count', prc_count):
 
-        has_key = mul_queue.get()
-        if has_key:
-            prc_file = multiprocessing.Process(target=self.__send_file_to_client(msg, mul_queue))
-            prc_file.start()
+            mul_queue = multiprocessing.Queue()
+            prc_redis = multiprocessing.Process(target=self.__check_for_key_in_redis, args=(msg, mul_queue))
+            prc_redis.start()
+
+            has_key = mul_queue.get()
+            if has_key:
+                prc_file = multiprocessing.Process(target=self.__send_file_to_client(msg, mul_queue))
+                prc_file.start()
 
     def __remove_dir(self, dir_name: str):
 
