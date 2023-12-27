09eec552b5767b0b6b7559611a47db204e26de7a
chimchimster <jorddyishere@gmail.com>
2023-12-27 12:59:43 +0600

    resolving bugs with zombie procesess in work

--- server/namespace/hook.py
+++ server/namespace/hook.py
@@ -1,12 +1,11 @@
 import time
-import shutil
 import os.path
 import multiprocessing
 
 from flask_socketio import Namespace, emit
 
 from reports.redis_api.api import ReportStorageRedisAPI
-from modules.logs.handlers import LokiLogger
+from reports.modules.logs.handlers import LokiLogger
 
 
 class SocketWebHookNamespace(Namespace):
@@ -25,8 +24,6 @@ class SocketWebHookNamespace(Namespace):
 
         if cls.r_con is None:
             cls.r_con = ReportStorageRedisAPI()
-        if cls.sema is None:
-            cls.sema = multiprocessing.Semaphore(3)
 
         return super().__new__(cls)
 
@@ -42,39 +39,33 @@ class SocketWebHookNamespace(Namespace):
 
     def __check_for_key_in_redis(self, msg: str, q: multiprocessing.Queue, timeout: int = 5000) -> bool:
 
-        with LokiLogger('Trying to find in Redis', report_id=msg):
-            with self.sema:
-                if self.r_con is not None:
-                    start = end = time.time()
-                    while end - start < timeout:
-                        has_key = self.r_con.connection.get(msg)
-                        if has_key:
-                            q.put(True)
-                            return True
-                        end = time.time()
-                q.put(False)
-                return False
-
-    def __clean_redis_up(self, key: str):
-
-        self.r_con.connection.delete(key)
+        with LokiLogger('Trying to find key in Redis', report_id=msg):
+            if self.r_con is not None:
+                start = end = time.time()
+                while end - start < timeout:
+                    has_key = self.r_con.connection.getdel(msg)
+                    if has_key:
+                        q.put(True)
+                        return True
+                    end = time.time()
+            q.put(False)
+            return False
 
     def __send_file_to_client(self, filename: str, q: multiprocessing.Queue):
 
         with LokiLogger('Sending file to client', report_id=filename):
-            with self.sema:
-                try:
-                    with open(
-                            os.path.join(
-                                self.path_to_file_dir,
-                                filename,
-                                filename + '.docx',
-                            ), 'rb') as file:
-                        file_data = file.read()
-                        emit('message', {'file_data': file_data})
-                        q.put(True)
-                except FileNotFoundError:
-                    q.put(False)
+            try:
+                with open(
+                        os.path.join(
+                            self.path_to_file_dir,
+                            filename,
+                            filename + '.docx',
+                        ), 'rb') as file:
+                    file_data = file.read()
+                    emit('message', {'file_data': file_data})
+                    q.put(True)
+            except FileNotFoundError:
+                q.put(False)
 
     def __fork_on_message(self, msg: str):
 
@@ -88,7 +79,5 @@ class SocketWebHookNamespace(Namespace):
 
             has_key = mul_queue.get()
             if has_key:
-                # prc_cleanup = multiprocessing.Process(target=self.__clean_redis_up(msg))
                 prc_file = multiprocessing.Process(target=self.__send_file_to_client(msg, mul_queue))
-                # prc_cleanup.start()
                 prc_file.start()
