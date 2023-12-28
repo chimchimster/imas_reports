85c3d84c8782e148387d0b25d040f2257eb0f25d
chimchimster <jorddyishere@gmail.com>
2023-12-28 15:20:06 +0600

    refactored and deleted logic via multiprocessing changed with threading

--- server/namespace/hook.py
+++ server/namespace/hook.py
@@ -1,7 +1,7 @@
+import queue
 import time
 import os.path
-import multiprocessing
-import multiprocessing.managers
+import threading
 
 from flask_socketio import Namespace, emit, call
 
@@ -28,6 +28,7 @@ class SocketWebHookNamespace(Namespace):
         return super().__new__(cls)
 
     def on_connect(self):
+        print(self.__dict__)
         print(f"Присоеденился клиент")
 
     def on_disconnect(self):
@@ -35,21 +36,22 @@ class SocketWebHookNamespace(Namespace):
 
     def on_message(self, msg: str):
 
-        multiprocessing.Process(target=self.__fork_on_message(msg,)).start()
+        threading.Thread(target=self.__fork_on_message(msg,)).start()
 
-    def __check_for_key_in_redis(self, msg: str, q: multiprocessing.Queue, timeout: int = 5000) -> bool:
+    def __check_for_key_in_redis(self, msg: str, q: queue.Queue, timeout: int = 5000) -> bool:
 
         with LokiLogger('Trying to find key in Redis', report_id=msg):
             if self.r_con is not None:
                 start = end = time.time()
                 while end - start < timeout:
-                    has_key = self.r_con.connection.get(msg)
-                    if has_key:
-                        q.put(True)
-                        with LokiLogger('Key found in Redis', msg):
+                    has_key = self.r_con.connection.getdel(msg)
+                    if has_key is not None:
+                        has_key = has_key.decode('utf-8')
+                        q.put(has_key)
+                        with LokiLogger('Key found in Redis', has_key, report_id=msg):
                             return True
                     end = time.time()
-            q.put(False)
+            q.put(None)
             with LokiLogger('Key doesnt found in Redis', msg):
                 return False
 
@@ -70,21 +72,17 @@ class SocketWebHookNamespace(Namespace):
 
     def __fork_on_message(self, msg: str):
 
-        redis_queue = multiprocessing.Queue()
-        prc_redis = multiprocessing.Process(target=self.__check_for_key_in_redis, args=(msg, redis_queue))
-        prc_redis.start()
+        redis_queue = queue.Queue()
+        prc_redis = threading.Thread(target=self.__check_for_key_in_redis, args=(msg, redis_queue))
+        prc_redis.run()
 
-        has_key = None
-        while redis_queue.empty():
-            if not redis_queue.empty():
-                has_key = redis_queue.get()
+        while not redis_queue.empty():
+
+            has_key = redis_queue.get()
+
+            if has_key not in ('error', None):
+                prc_file = threading.Thread(target=self.__send_file_to_client, args=(msg,))
+                prc_file.run()
                 break
-
-        if prc_redis.is_alive():
-            prc_redis.terminate()
-
-        if has_key:
-            prc_file = multiprocessing.Process(target=self.__send_file_to_client(msg))
-            prc_file.start()
-
-
+            else:
+                return
