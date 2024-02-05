0367f41d312de260820309a22330920cb118669f
chimchimster <jorddyishere@gmail.com>
2024-02-05 17:48:44 +0600

    refactoring

--- modules/apps/excel/utils/report_generator.py
+++ modules/apps/excel/utils/report_generator.py
@@ -14,34 +14,7 @@ class ExcelReportGenerator:
             if order := setting.get('order'):
                 return DataSorter(self._data, order).sort_data()
 
-    @staticmethod
-    def __prepare_mass_media_table_data(
-            mass_media_data,
-            mass_media_table_settings
-    ):
-
-        mapping = {
-            'title': 'title',
-            'content': 'full_text',
-            'date': 'not_date',
-            'resource_name': 'resource',
-            'resource_page_url': 'resource_link',
-            'link': 'news_link',
-            'sentiment': 'sentiment',
-            'category': 'category',
-        }
-
-        if columns := mass_media_table_settings.get('columns'):
-            for column in columns:
-                pass
-
-
     def generate_excel_document(self):
 
         data = self.__sort_data()
-
-        if not data:
-            return
-
-        dataframe = pd.DataFrame(data)
-        print(dataframe)
+        print(data)
