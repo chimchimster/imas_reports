4af9180f6de89884d728ab8a21f80b7aec56dd8a
chimchimster <jorddyishere@gmail.com>
2024-02-09 13:01:55 +0600

    fixed logic with retrieving messages from broker on server side, found mistake in websocket emit function

--- server/namespace/hook.py
+++ server/namespace/hook.py
@@ -1,3 +1,4 @@
+import queue
 import time
 import os.path
 import threading
@@ -43,10 +44,11 @@ class SocketWebHookNamespace(Namespace):
     def __check_for_key_in_redis(
             self,
             msg: str,
-            event: threading.Event,
+            redis_event: threading.Event,
+            redis_q: queue.Queue,
             timeout: int = 5000,
             receive_type: str = ''
-    ) -> bool:
+    ) -> None:
 
         with LokiLogger('Trying to find key in Redis' + ' ' + receive_type, report_id=msg):
             if self.r_con is not None:
@@ -54,43 +56,78 @@ class SocketWebHookNamespace(Namespace):
                 while end - start < timeout:
                     has_key = self.r_con.connection.getdel(msg)
                     if has_key is not None:
+                        print(has_key)
                         has_key = has_key.decode('utf-8')
-                        event.set()
                         with LokiLogger('Key found in Redis', has_key, report_id=msg):
-                            return True
+                            if receive_type == 'second time':
+                                redis_q.put(has_key)
+                            redis_event.set()
+                            return
                     end = time.time()
 
             with LokiLogger('Key doesnt found in Redis', msg):
-                return False
+                redis_q.put('error')
 
-    def __send_file_to_client(self, file_uuid: str, file_ready_event: threading.Event):
+    def __send_file_to_client(
+            self,
+            file_uuid: str,
+            status: str
+    ) -> None:
 
         with LokiLogger('Sending file to client', report_id=file_uuid):
 
-            if self.__check_for_key_in_redis(file_uuid, file_ready_event, receive_type='second time'):
+            if status is None or status == 'error':
+                emit('message', {'file_data': b'Error during constructing report'})
+            else:
                 try:
                     response = requests.get(self.STORAGE_API_ENDPOINT + '/api/v1/files/file/download/', params={
                         'file_uuid': file_uuid
                     })
-                except requests.exceptions.RequestException:
-                    raise requests.exceptions.RequestException
+                except requests.exceptions.RequestException as e:
+                    raise e
 
                 emit('message', {'file_data': response.content}, callback=lambda: print('OKЭY'))
 
-    def __fork_on_message(self, msg: str):
-
-        redis_event = threading.Event()
-        file_ready_event = threading.Event()
-
+    def __start_redis_thr(
+            self,
+            msg: str,
+            redis_event: threading.Event,
+            redis_q: queue.Queue,
+            receive_type: str
+    ) -> threading.Thread:
         thr_redis = threading.Thread(
             target=self.__check_for_key_in_redis,
-            args=(msg, redis_event),
-            kwargs={'receive_type': 'first time'}
+            args=(msg, redis_event, redis_q),
+            kwargs={'receive_type': receive_type}
         )
-        thr_redis.run()
+        thr_redis.start()
+        return thr_redis
 
-        while True:
+    def __fork_on_message(self, msg: str, timeout: int = 5000):
+
+        redis_event, redis_q = threading.Event(), queue.Queue()
+
+        thr_r1 = self.__start_redis_thr(msg, redis_event, redis_q, receive_type='first time')
+        thr_r1.join()
+
+        start, end = time.time(), time.time()
+        while end - start < timeout:
+
             if redis_event.is_set():
-                thr_send_file = threading.Thread(target=self.__send_file_to_client, args=(msg, file_ready_event))
-                thr_send_file.run()
-                break
+
+                redis_event.clear()
+
+                self.__start_redis_thr(msg, redis_event, redis_q, receive_type='second time')
+
+                while end - start < timeout:
+
+                    status = redis_q.get()
+
+                    if status is not None or status != 'error':
+                        thr_send_file = threading.Thread(
+                            target=self.__send_file_to_client,
+                            args=(msg, status)
+                        )
+                        thr_send_file.start()
+                        break
+                    end = time.time()
